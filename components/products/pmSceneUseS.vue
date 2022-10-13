8446bca639ac67b0dadf857e7e8e7277ac9ee8b8
litthy <litthy@qq.com>
2022-10-13 17:41:46 +0800

    fix: type

--- components/products/pmSceneUseS.vue
+++ components/products/pmSceneUseS.vue
@@ -1,24 +1,30 @@
 <template>
     <div class="m-p-SceneuseS">
         <div class="wp">
-            <h2>{{pmSceneUseSTil}}</h2>
+            <h2>{{ pmSceneUseSTil }}</h2>
             <div class="m-Scontent">
                 <div class="m-item" :class="pmSceneUseS.imgR">
                     <span class="u-img" :style="`backgroundImage: url(${pmSceneUseS.imgSrc})`"></span>
-                    <span class="u-title"><b>{{ pmSceneUseS.title }}</b>{{pmSceneUseS.titletag}}</span>
-                    <div class="u-content" v-for="(item,uindex) in pmSceneUseS.list" :key="uindex">
-                        <span class="u-til">{{item.til}}</span>
-                        <span class="u-desc">{{item.desc}}</span>
+                    <span class="u-title"
+                        ><b>{{ pmSceneUseS.title }}</b
+                        >{{ pmSceneUseS.titletag }}</span
+                    >
+                    <div class="u-content" v-for="(item, uindex) in pmSceneUseS.list" :key="uindex">
+                        <span class="u-til">{{ item.til }}</span>
+                        <span class="u-desc">{{ item.desc }}</span>
                     </div>
                 </div>
             </div>
             <div class="m-Scontent" v-if="pmSceneUseSTrue">
                 <div class="m-item">
                     <span class="u-img" :style="`backgroundImage: url(${pmSceneUseS01.imgSrc})`"></span>
-                    <span class="u-title"><b>{{ pmSceneUseS01.title }}</b>{{pmSceneUseS01.titletag}}</span>
-                    <div class="u-content" v-for="(item,uindex) in pmSceneUseS01.list" :key="uindex">
-                        <span class="u-til">{{item.til}}</span>
-                        <span class="u-desc">{{item.desc}}</span>
+                    <span class="u-title"
+                        ><b>{{ pmSceneUseS01.title }}</b
+                        >{{ pmSceneUseS01.titletag }}</span
+                    >
+                    <div class="u-content" v-for="(item, uindex) in pmSceneUseS01.list" :key="uindex">
+                        <span class="u-til">{{ item.til }}</span>
+                        <span class="u-desc">{{ item.desc }}</span>
                     </div>
                 </div>
             </div>
@@ -31,20 +37,14 @@ export default {
     props: {
         pmSceneUseS: Object,
         pmSceneUseSTil: String,
-        pmSceneUseSTrue: String,
-        pmSceneUseS01: Object
+        pmSceneUseSTrue: Boolean,
+        pmSceneUseS01: Object,
     },
     data() {
-        return {
-
-        };
+        return {};
     },
-    computed: {
-
-    },
-    methods: {
-
-    },
+    computed: {},
+    methods: {},
 };
 </script>
 
