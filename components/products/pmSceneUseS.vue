7ff98446406095279f91056d214cf1b849e405f1
litthy <litthy@qq.com>
2022-10-13 18:02:06 +0800

    fix：bug 修复

--- components/products/pmSceneUseS.vue
+++ components/products/pmSceneUseS.vue
@@ -5,10 +5,10 @@
             <div class="m-Scontent">
                 <div class="m-item" :class="pmSceneUseS.imgR">
                     <span class="u-img" :style="`backgroundImage: url(${pmSceneUseS.imgSrc})`"></span>
-                    <span class="u-title"
-                        ><b>{{ pmSceneUseS.title }}</b
-                        >{{ pmSceneUseS.titletag }}</span
-                    >
+                    <span class="u-title">
+                        <span class="u-bold">{{ pmSceneUseS.title }}</span>
+                        <span v-if="pmSceneUseS.titletag"> {{ pmSceneUseS.titletag }}</span>
+                    </span>
                     <div class="u-content" v-for="(item, uindex) in pmSceneUseS.list" :key="uindex">
                         <span class="u-til">{{ item.til }}</span>
                         <span class="u-desc">{{ item.desc }}</span>
@@ -18,10 +18,10 @@
             <div class="m-Scontent" v-if="pmSceneUseSTrue">
                 <div class="m-item">
                     <span class="u-img" :style="`backgroundImage: url(${pmSceneUseS01.imgSrc})`"></span>
-                    <span class="u-title"
-                        ><b>{{ pmSceneUseS01.title }}</b
-                        >{{ pmSceneUseS01.titletag }}</span
-                    >
+                    <span class="u-title">
+                        <span class="u-bold">{{ pmSceneUseS01.title }}</span>
+                        {{ pmSceneUseS01.titletag }}
+                    </span>
                     <div class="u-content" v-for="(item, uindex) in pmSceneUseS01.list" :key="uindex">
                         <span class="u-til">{{ item.til }}</span>
                         <span class="u-desc">{{ item.desc }}</span>
