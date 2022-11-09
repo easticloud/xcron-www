645117314a8b59da3ced558e6ac03ffa1a7a1a36
litthy <litthy@qq.com>
2022-11-09 17:46:05 +0800

    feat: style

--- components/index/menu.vue
+++ components/index/menu.vue
@@ -55,173 +55,13 @@
     </div>
 </template>
 <script>
+import { menu } from "@/assets/data/index.json";
 export default {
     props: ["type"],
     data() {
         return {
             index: 0,
             search: "",
-            production: [
-                {
-                    title: "人工智能",
-                    children: [
-                        {
-                            title: "智能AI边缘盒子",
-                            desc: "提供识别、抓拍、对比、报警等服务",
-                            link: "/products/products_ai",
-                        },
-                        {
-                            title: "AI在线服务",
-                            desc: "适用于图像识别、自然语言处理等...",
-                            link: "/products/products_ais",
-                        },
-                    ],
-                },
-                {
-                    title: "区块链",
-                    children: [
-                        {
-                            title: "区块链服务",
-                            desc: "高性能、高安全的区块链技术平台服务",
-                            link: "/products/products_bcs",
-                        },
-                        {
-                            title: "可信跨链服务",
-                            desc: "实现不同区块链内核的可信互联互通",
-                            link: "/products/products_tcs",
-                        },
-                    ],
-                },
-                {
-                    title: "云计算",
-                    children: [
-                        {
-                            title: "弹性云服务器",
-                            desc: "可随时自动获取、弹性伸缩的云服务器",
-                            link: "/products/products_ecs",
-                        },
-                        {
-                            title: "裸金属服务器",
-                            desc: "高性能、高安全的云上物理服务器",
-                            link: "/products/products_bms",
-                        },
-                        {
-                            title: "专属主机",
-                            desc: "专属物理主机创建的云服务器",
-                            link: "/products/products_dh",
-                        },
-                    ],
-                },
-                {
-                    title: "大数据",
-                    children: [
-                        {
-                            title: "云数据仓库",
-                            desc: "极致性能、稳定、按需扩展的数据仓库服务",
-                            link: "/products/products_cdw",
-                        },
-                        {
-                            title: "数据接入服务",
-                            desc: "实时数据接入、处理、转储服务",
-                            link: "/products/products_dis",
-                        },
-                        {
-                            title: "数据可视化",
-                            desc: "提供可视化组件定制和应用数据大屏",
-                            link: "/products/products_dlv",
-                        },
-                    ],
-                },
-                {
-                    title: "网络",
-                    children: [
-                        { title: "虚拟私有云", desc: "隔离的、私密的虚拟网络环境", link: "/products/products_vpc" },
-                        {
-                            title: "云专线",
-                            desc: "搭建本地数据中心与VPC间的专属连接通道",
-                            link: "/products/products_dc",
-                        },
-                        { title: "全球加速", desc: "提供SLA稳定的加速传输", link: "/products/products_ga" },
-                    ],
-                },
-                {
-                    title: "智能边缘",
-                    children: [
-                        {
-                            title: "全站加速",
-                            desc: "提升网络传输的性能、稳定性和访问体验",
-                            link: "/products/products_wsa",
-                        },
-                        {
-                            title: "智能边缘云",
-                            desc: "部署在距用户更近的位置，提供低时延体验",
-                            link: "/products/products_iec",
-                        },
-                    ],
-                },
-                {
-                    title: "终端",
-                    children: [
-                        {
-                            title: "终端安全防护",
-                            desc: "联动网络侧的安全防御能力，为企业构建由网到端的全面闭环防护",
-                            link: "/products/products_tsp",
-                        },
-                    ],
-                },
-                {
-                    title: "安全",
-                    children: [
-                        { title: "云防火墙", desc: "网络流量管控与入侵安全防护", link: "/products/products_cf" },
-                        { title: "企业主机安全", desc: "服务器贴身安全管家", link: "/products/products_hss" },
-                        { title: "数据加密服务", desc: "云上数据加密和密钥托管服务", link: "/products/products_dew" },
-                    ],
-                },
-            ],
-            solution: [
-                {
-                    label: "新零售行业解决方案",
-                    desc: "要素整合、精准营销、高效果运营",
-                    icon: "/images/index/solution/icon-1.png",
-                    link: "/programme/new_retail",
-                },
-                {
-                    label: "运营商行业解决方案",
-                    desc: "网络变现、业务创新、运营敏捷",
-                    icon: "/images/index/solution/icon-2.png",
-                    link: "/programme/operators",
-                },
-                {
-                    label: "数字政府解决方案",
-                    desc: "服务能力向移动化和智能化发展",
-                    icon: "/images/index/solution/icon-3.png",
-                    link: "/programme/government",
-                },
-                {
-                    label: "金融行业解决方案",
-                    desc: "敏捷创新、智能升级、数字化转型",
-                    icon: "/images/index/solution/icon-4.png",
-                    link: "/programme/finance",
-                },
-                {
-                    label: "迁移上云解决方案",
-                    desc: "提供ERP系统上云一站式服务",
-                    icon: "/images/index/solution/icon-5.png",
-                    link: "/programme/cloud",
-                },
-                {
-                    label: "TIk Tok解决方案",
-                    desc: "解决个人/企业Tiktok问题",
-                    icon: "/images/index/solution/icon-6.png",
-                    link: "/programme/media",
-                },
-                {
-                    label: "其他行业解决方案",
-                    desc: "能源丨医疗健康丨教育丨交通物流",
-                    icon: "/images/index/solution/icon-7.png",
-                    link: "/programme/other?key=energy",
-                },
-            ],
         };
     },
     computed: {
@@ -233,6 +73,12 @@ export default {
             return this.search
                 ? flattenChildren.filter((item) => item.title.includes(this.search))
                 : list[this.index].children;
+        },
+        solution() {
+            return menu.solution;
+        },
+        production() {
+            return menu.production;
         },
     },
     watch: {
--- pages/programme/index.vue
+++ pages/programme/index.vue
@@ -5,7 +5,31 @@
         <!-- banner -->
         <banner bannerKey="programme" />
 
-        <div class="m-page-content"></div>
+        <div class="m-page-content">
+            <!-- 热门方案展示 -->
+            <div class="m-page-show">
+                <h2>热门方案展示</h2>
+                <div class="u-page-content wp">
+                    <div
+                        class="u-item"
+                        :class="`u-item-${i + 1}`"
+                        v-for="(item, i) in list"
+                        :key="i"
+                        :style="style(item)"
+                    >
+                        <span class="u-title">{{ item.title }}</span>
+                        <span class="u-desc">{{ item.desc }}</span>
+                    </div>
+                </div>
+            </div>
+            <!-- 行业解决方案 -->
+            <div class="m-page-solution">
+                <h2>行业解决方案</h2>
+                <div class="wp">
+                    <index-menu class="u-page-solution" type="programme"></index-menu>
+                </div>
+            </div>
+        </div>
         <!-- 服务支持 -->
         <products-pmService></products-pmService>
         <!-- 公共底部 -->
@@ -15,11 +39,26 @@
 
 <script>
 import banner from "@/components/page/banner.vue";
+import { show } from "@/assets/data/product.json";
 export default {
     name: "ProgrammePage",
     components: { banner },
+    computed: {
+        list() {
+            return show;
+        },
+    },
+    methods: {
+        style(item) {
+            if (item.color) return { backgroundColor: item.color };
+            if (item.img) return { backgroundImage: `url(${item.img})` };
+            return {};
+        },
+    },
 };
 </script>
 <style lang="less">
 @import "~@/assets/css/page/index.less";
+
+@import "~@/assets/css/page/programme.less";
 </style>
