d21ab9a843a68d169ebb9a30af567d9330f81eed
Kaviilee <jiaweilee95@126.com>
2025-01-06 16:39:14 +0800

    fix: kv

--- settings.js
+++ settings.js
@@ -1,10 +1,9 @@
 module.exports = {
     // SEO
     title: "实点实分",
-    description: "实点实分网络科技有限公司",
+    description: "实点实分是一家是一家国内互联网数据领域的专业服务提供商，为传统企业转型互联网和互联网企业提供数据接入及大数据分析应用。",
 
-    // URL
-    __cdn: "https://console.cnyixun.com/easticloud/",
+    __img: "https://img.easticloud.com/",
 
     // API
     API:{
